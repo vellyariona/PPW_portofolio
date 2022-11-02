f7a0b5a8a25850349107642ea1b1b2f1a9e70d55
vellyariona <vellya.riona@mail.ugm.ac.id>
2022-11-02 17:41:14 +0700

    email dan reset email

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,44 +1,10 @@
 @extends('layout/main')
 
 @section('container')
-<section id="home" class="article text-center">
-            <div class="container shadow-ms">
-                <div class="row text-center">   
-                    <div class="col">
-                        <h2>Hi, I'am Vellya Riona👋</h2>
-                    </div>
-                </div>
-                <div class="row justify-content-center text-center mt-3 text-dark">
-                    <div class="col-md-4 mb-3">
-                        <div class="card shadow p"><a class="card-box" href="/about">
-                            <div class="card-body">
-                                <h5 class="card-title">About</h5>
-                                <p class="card-text">Detail >></p>
-                            </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <div class="card shadow p"><a class="card-box" href="/education">
-                            <div class="card-body">
-                                <h5 class="card-title">Education</h5>
-                                <p class="card-text">Detail >></p>
-                            </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <div class="card shadow p"><a class="card-box" href="/projects">
-                            <div class="card-body">
-                                <h5 class="card-title">Projects</h5>
-                                <p class="card-text">Detail >></p>
-                            </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!--akhir article-->
-@endsection
 
+<div class="mt-5 text-center">
+    <h1>UTS WEB 2 - Vellya Riona</h1>
+</div>
+
+
+@endsection
