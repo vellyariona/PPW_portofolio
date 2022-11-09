fe223c711f75f81742d410672e4d80b460232f5b
vellyariona <vellya.riona@mail.ugm.ac.id>
2022-11-10 04:52:50 +0700

    file upload

---
+++ kirim-email.blade.php
@@ -0,0 +1,32 @@
+<div class = "row justify-content-center">
+    <h3 class = "text-center my-2"> Tutorial Queue</h3>
+    <div class = "col-md-4 p-4">
+        {{-- send email --}}
+        @if (session('status'))
+        <div class = "alert alert-primary" role="alert">
+        {{ session('status')}}
+    </div>
+
+    @endif
+    <form action = "{{ route('post-email') }}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="name">Nama</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Nama">
+        </div>
+        <div class ="form-group my-3">
+            <label for="email">Email Tujuan</label>
+            <input type="email" class="form-control" name="email" id="email" placeholder="Email Tujuan">
+        </div>
+        <div class ="form-group my-3">
+            <label for="name">Body Deskripsi<label>
+            <textarea name="body" class="form-control" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary">Kirim Email</button>
+        </div>
+    </div>
+</div>
+
+    
+
